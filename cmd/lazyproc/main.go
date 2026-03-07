cd129474b46e6c86de91b83d9ae8ac00829af176
blucin <blucin@users.noreply.github.com>
2026-03-07 15:43:40 +0530

    Add debug logging

--- cmd/lazyproc/main.go
+++ cmd/lazyproc/main.go
@@ -11,6 +11,15 @@ import (
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	configPath := flag.String("config", "lazyproc.yaml", "path to config file")
 	flag.Parse()
 
